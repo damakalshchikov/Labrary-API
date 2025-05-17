694cbf85cf0bedd1c1042f2132f3bd200f516b77
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-18 01:33:44 +0300

    Создана модель Author для создания соответствующей сущности в базе данных

---
+++ Solution.Sendy.CSharp.TestTask/DataBase/Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace Solution.Sendy.CSharp.TestTask.DataBase.Models;
+
+public class Author
+{
+    public int AuthorId { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
