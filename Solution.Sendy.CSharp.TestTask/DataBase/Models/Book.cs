74e2291ac6a98b012ba4efd9a8d26bb370b8e339
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-18 01:33:56 +0300

    Создана модель Book для создания соответствующей сущности в базе данных

---
+++ Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Solution.Sendy.CSharp.TestTask.DataBase.Models;
+
+public class Book
+{
+    public int BookId { get; set; }
+    public string Title { get; set; } = null!;
+
+    public int AuthorId { get; set; }
+    public Author Author { get; set; } = null!;
+}
