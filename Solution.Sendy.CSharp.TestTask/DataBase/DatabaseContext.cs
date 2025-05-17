d381b6f576ec1f6b04f847b77e2bc6368b46f5a5
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-18 01:35:31 +0300

    Определён DatabaseContext для взаимодействия с базой данных

---
+++ Solution.Sendy.CSharp.TestTask/DataBase/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Solution.Sendy.CSharp.TestTask.DataBase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Solution.Sendy.CSharp.TestTask.DataBase;
+
+public class DatabaseContext : DbContext
+{
+    // Создаём таблицы по моделям
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options)
+        : base(options)
+    {
+    }
+}
