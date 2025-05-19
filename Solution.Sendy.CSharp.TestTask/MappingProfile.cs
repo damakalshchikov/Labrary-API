bac7ccd84e6d80c485c1a7514f6c816498176ca5
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-19 21:42:42 +0300

    Настройка маппинга

---
+++ Solution.Sendy.CSharp.TestTask/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Solution.Sendy.CSharp.TestTask.DTOs;
+using Solution.Sendy.CSharp.TestTask.DataBase.Models;
+
+namespace Solution.Sendy.CSharp.TestTask;
+
+public class MappingProfile  : Profile
+{
+    public MappingProfile ()
+    {
+        // Author из БД (EF Core) <-> AutoMapper
+        CreateMap<Author, AuthorDTO>();
+        CreateMap<CreateAuthorDTO, Author>();
+        CreateMap<UpdateAuthorDTO, Author>();
+
+        // Book из БД (EF Core) <-> AutoMapper
+        CreateMap<Book, BookDTO>();
+        CreateMap<CreateBookDTO, Book>();
+        CreateMap<UpdateBookDTO, Book>();
+    }
+}
