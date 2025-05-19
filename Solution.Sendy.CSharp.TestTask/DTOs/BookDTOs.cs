1c69e74daedd25265668e6becf088c4897159ab3
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-19 21:41:58 +0300

    Определены объекты обмена данными между БД(модель Book) и различными слоями приложения

---
+++ Solution.Sendy.CSharp.TestTask/DTOs/BookDTOs.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Solution.Sendy.CSharp.TestTask.DTOs;
+
+public class BookDTO
+{
+    public int BookId { get; set; }
+    public string Title { get; set; } = null!;
+    public int AuthorId { get; set; }
+    public AuthorDTO Author { get; set; } = null!;
+}
+
+public class CreateBookDTO
+{
+    [Required(ErrorMessage = "Название книги - обязательное поле для заполнения")]
+    public string Title { get; set; } = null!;
+
+    [Required(ErrorMessage = "ID автора - обязательное поле для заполнения")]
+    public int AuthorId { get; set; }
+}
+
+public class UpdateBookDTO
+{
+    [Required(ErrorMessage = "Название книги - обязательное поле для заполнения")]
+    public string Title { get; set; } = null!;
+
+    [Required(ErrorMessage = "ID автора - обязательное поле для заполнения")]
+    public int AuthorId { get; set; }
+}
