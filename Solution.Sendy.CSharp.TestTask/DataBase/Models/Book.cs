af3b16141b24d359a7677eeb04503094a9d51906
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-21 00:47:52 +0300

    Обновление модели Book

--- Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
+++ Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
@@ -4,7 +4,6 @@ public class Book
 {
     public int BookId { get; set; }
     public string? Title { get; set; }
-
-    public int AuthorId { get; set; }
+    public int? AuthorId { get; set; }
     public Author? Author { get; set; }
 }
