5daefce9d19d642b27e5534ed8a63796a0e8c3e1
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-20 23:36:12 +0300

    Обновление моделей, чтобы неключевые атрибуты могли принимать NULL по умолчанию

--- Solution.Sendy.CSharp.TestTask/DataBase/Models/Author.cs
+++ Solution.Sendy.CSharp.TestTask/DataBase/Models/Author.cs
@@ -3,7 +3,7 @@
 public class Author
 {
     public int AuthorId { get; set; }
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public string Email { get; set; } = null!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
 }
--- Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
+++ Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
@@ -3,8 +3,8 @@
 public class Book
 {
     public int BookId { get; set; }
-    public string Title { get; set; } = null!;
+    public string? Title { get; set; }
 
     public int AuthorId { get; set; }
-    public Author Author { get; set; } = null!;
+    public Author? Author { get; set; }
 }
