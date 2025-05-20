865df9284ea94b90d16d7255cdd84640fedb5678
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-20 23:06:19 +0300

    Подключаем контроллеры в приложении и настраиваем маршруты

--- Solution.Sendy.CSharp.TestTask/Program.cs
+++ Solution.Sendy.CSharp.TestTask/Program.cs
@@ -14,6 +14,11 @@ builder.Services.AddDbContext<DatabaseContext>(options
 // Регистрируем AutoMapper
 builder.Services.AddAutoMapper(typeof(Program));
 
+// Добавляем контроллеры
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,28 +29,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Добавляем маршрутизацию для контроллеров
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
