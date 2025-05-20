ee2f89e7d411787fc90e9e8c8cb890b569b0a1fd
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-21 00:48:28 +0300

    Обновление DTO-объекта Author

--- Solution.Sendy.CSharp.TestTask/DTOs/AuthorDTOs.cs
+++ Solution.Sendy.CSharp.TestTask/DTOs/AuthorDTOs.cs
@@ -1,33 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Solution.Sendy.CSharp.TestTask.DTOs;
+﻿namespace Solution.Sendy.CSharp.TestTask.DTOs;
 
 public class AuthorDTO
 {
     public int AuthorId { get; set; }
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public string Email { get; set; } = null!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
 }
 
 public class CreateAuthorDTO
 {
-    [Required(ErrorMessage = "Имя автора - обязательное поле для заполнения")]
-    public string FirstName { get; set; } = null!;
-
-    [Required(ErrorMessage = "Фамилия автора - обязательное поле заполнения")]
-    public string LastName { get; set; } = null!;
-
-    public string Email { get; set; } = null!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
 }
 
 public class UpdateAuthorDTO
 {
-    [Required(ErrorMessage = "Имя автора - обязательное поле для заполнения")]
-    public string FirstName { get; set; } = null!;
-
-    [Required(ErrorMessage = "Фамилия автора - обязательное поле для заполнения")]
-    public string LastName { get; set; } = null!;
-
-    public string Email { get; set; } = null!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
 }
