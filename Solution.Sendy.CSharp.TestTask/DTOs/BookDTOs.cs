0def568b5e3afc87d432f51884dec8791191f8bc
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-25 05:02:16 +0300

    Добавлена валидация данных DTO-объектов типа Book

--- Solution.Sendy.CSharp.TestTask/DTOs/BookDTOs.cs
+++ Solution.Sendy.CSharp.TestTask/DTOs/BookDTOs.cs
@@ -1,4 +1,6 @@
-﻿namespace Solution.Sendy.CSharp.TestTask.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Solution.Sendy.CSharp.TestTask.DTOs;
 
 public class BookDTO
 {
@@ -10,12 +12,22 @@ public class BookDTO
 
 public class CreateBookDTO
 {
-    public string? Title { get; set; }
-    public int? AuthorId { get; set; }
+    [Required(ErrorMessage = "Название книги  обязательно")]
+    [StringLength(50, MinimumLength = 5, ErrorMessage = "Название книги должно быть от 5 до 50 символов")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Id автора обязательно")]
+    [Range(1, int.MaxValue, ErrorMessage = "Id автора должен быть больше 0")]
+    public int AuthorId { get; set; } = 1;
 }
 
 public class UpdateBookDTO
 {
-    public string? Title { get; set; }
-    public int? AuthorId { get; set; }
+    [Required(ErrorMessage = "Название книги  обязательно")]
+    [StringLength(50, MinimumLength = 5, ErrorMessage = "Название книги должно быть от 5 до 50 символов")]
+    public string Title { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Id автора обязательно")]
+    [Range(1, int.MaxValue, ErrorMessage = "Id автора должен быть больше 0")]
+    public int AuthorId { get; set; } = 1;
 }
