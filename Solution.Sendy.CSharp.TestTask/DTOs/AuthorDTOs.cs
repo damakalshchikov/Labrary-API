e0852ed1261a5dce7189294ada58ac5b091f01d5
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-25 05:01:44 +0300

    Добавлена базовая валиация данных для DTO-объектов типа Author

--- Solution.Sendy.CSharp.TestTask/DTOs/AuthorDTOs.cs
+++ Solution.Sendy.CSharp.TestTask/DTOs/AuthorDTOs.cs
@@ -1,4 +1,6 @@
-﻿namespace Solution.Sendy.CSharp.TestTask.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Solution.Sendy.CSharp.TestTask.DTOs;
 
 public class AuthorDTO
 {
@@ -10,14 +12,30 @@ public class AuthorDTO
 
 public class CreateAuthorDTO
 {
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Email { get; set; }
+    [Required(ErrorMessage = "Имя автора  обязательно")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Имя автора должно быть от 2 до 50 символов")]
+    public string FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Фамилия автора  обязательна")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Фамилия автора должна быть от 2 до 50 символов")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email  обязателен")]
+    [EmailAddress(ErrorMessage = "Email должен быть валидным")]
+    public string Email { get; set; } = string.Empty;
 }
 
 public class UpdateAuthorDTO
 {
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Email { get; set; }
+    [Required(ErrorMessage = "Имя автора  обязательно")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Имя автора должно быть от 2 до 50 символов")]
+    public string? FirstName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Фамилия автора  обязательна")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Фамилия автора должна быть от 2 до 50 символов")]
+    public string LastName { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Email  обязателен")]
+    [EmailAddress(ErrorMessage = "Email должен быть валидным")]
+    public string Email { get; set; } = string.Empty;
 }
