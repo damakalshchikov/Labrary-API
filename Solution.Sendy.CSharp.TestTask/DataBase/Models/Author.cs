a97abacd951615e1163dc2151ce7c7897b3e62c1
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-27 10:38:06 +0300

    Рефакторинг

--- Solution.Sendy.CSharp.TestTask/DataBase/Models/Author.cs
+++ Solution.Sendy.CSharp.TestTask/DataBase/Models/Author.cs
@@ -3,8 +3,11 @@
 public class Author
 {
     public int AuthorId { get; set; }
+
     public string FirstName { get; set; } = null!;
+
     public string LastName { get; set; } = null!;
+
     public string? Email { get; set; }
     
     public ICollection<Book> Books { get; set; } = new List<Book>();
--- Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
+++ Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
@@ -3,6 +3,7 @@
 public class Book
 {
     public int BookId { get; set; }
+
     public string Title { get; set; } = null!;
 
     [ForeignKey("Author")]
