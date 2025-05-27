4d46850613ac4a87a40ac8769e78a6eb91c0682b
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-27 10:37:47 +0300

    Добавлено свойство Author в модель Book для связи с автором

--- Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
+++ Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
@@ -4,5 +4,9 @@ public class Book
 {
     public int BookId { get; set; }
     public string Title { get; set; } = null!;
+
+    [ForeignKey("Author")]
     public int AuthorId { get; set; }
+    
+    public Author Author { get; set; } = null!;
 }
