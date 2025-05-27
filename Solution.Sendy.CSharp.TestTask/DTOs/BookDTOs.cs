4706113b1019cb75967bf23e40980706cac438fc
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-27 10:14:19 +0300

    Удалено свойство Author из модели Book из-за ненадобности

--- Solution.Sendy.CSharp.TestTask/DTOs/BookDTOs.cs
+++ Solution.Sendy.CSharp.TestTask/DTOs/BookDTOs.cs
@@ -7,7 +7,6 @@ public class BookDTO
     public int BookId { get; set; }
     public string? Title { get; set; }
     public int? AuthorId { get; set; }
-    public AuthorDTO? Author { get; set; }
 }
 
 public class CreateBookDTO
--- Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
+++ Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
@@ -5,5 +5,4 @@ public class Book
     public int BookId { get; set; }
     public string Title { get; set; } = null!;
     public int AuthorId { get; set; }
-    public Author Author { get; set; } = null!;
 }
