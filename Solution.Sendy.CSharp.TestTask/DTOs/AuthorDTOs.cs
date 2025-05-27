e4f17f1ca5ec748fc3d5bf9d4e249b89abd36180
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-27 10:29:34 +0300

    Рефакторинг

--- Solution.Sendy.CSharp.TestTask/DTOs/AuthorDTOs.cs
+++ Solution.Sendy.CSharp.TestTask/DTOs/AuthorDTOs.cs
@@ -5,8 +5,11 @@ namespace Solution.Sendy.CSharp.TestTask.DTOs;
 public class AuthorDTO
 {
     public int AuthorId { get; set; }
+
     public string? FirstName { get; set; }
+
     public string? LastName { get; set; }
+    
     public string? Email { get; set; }
 }
 
