8805f15700a65c845be9ab23b69fa2dec77949a3
Mikhail Damakalshchikov <damakalshchikov@gmail.com>
2025-05-27 09:35:07 +0300

    Установлены обязательные значения для свойств Title, AuthorId и Author в модели Book

--- Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
+++ Solution.Sendy.CSharp.TestTask/DataBase/Models/Book.cs
@@ -3,7 +3,7 @@
 public class Book
 {
     public int BookId { get; set; }
-    public string? Title { get; set; }
-    public int? AuthorId { get; set; }
-    public Author? Author { get; set; }
+    public string Title { get; set; } = null!;
+    public int AuthorId { get; set; }
+    public Author Author { get; set; } = null!;
 }
